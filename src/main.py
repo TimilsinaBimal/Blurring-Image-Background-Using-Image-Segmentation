65d0d6e4a64b8d6c3f7da880a080e67d466d96b9
TimilsinaBimal <timilsina.bml05@gmail.com>
2021-06-04 13:02:41 +0545

    model prediction feature

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 import os
 import pandas as pd
 from pathlib import Path
-from models.train_model import (
+from data.make_dataset import Dataset
+from models.models import SegNet, PSPNet, UNet, DeepLab, ImageSegmentation
+from models.predict import show_predictions, create_mask, predict
+from models.train import (
     train_segnet,
     train_deeplab,
     train_pspnet,
@@ -22,14 +25,19 @@ def train_model():
     model_selection = int(input("Your Choice: "))
     if model_selection == 1:
         model = train_segnet()
+
     elif model_selection == 2:
         model = train_unet()
+
     elif model_selection == 3:
         model = train_pspnet()
+
     elif model_selection == 4:
         model = train_deeplab()
+
     elif model_selection == 5:
         model = train_segmentation()
+
     df_file = model.__class__.__name__
     history = train(model, ROOT_DIR)
     df = pd.DataFrame(history.history)
@@ -37,9 +45,46 @@ def train_model():
               f"{df_file}_history.csv"), index=False)
 
 
+def predict_model():
+    print("Please Choose:")
+    print("1: Get Accuracy and Loss of test dataset.")
+    print("2. Get accuracy and Loss of test dataset with some results:")
+    choice = int(input("Your Choice: "))
+
+    BEST_MODEL = "segnet"
+    ROOT_DIR = Path(__file__).parent.parent
+
+    if BEST_MODEL == "unet":
+        model = train_unet()
+        model_path = os.path.join(ROOT_DIR, "models/unet/")
+    elif BEST_MODEL == "segnet":
+        model = train_segnet()
+        model_path = os.path.join(ROOT_DIR, "models/segnet/")
+    elif BEST_MODEL == "pspnet":
+        model = train_pspnet()
+        model_path = os.path.join(ROOT_DIR, "models/pspnet/")
+
+    model.load_weights(model_path)
+
+    dataset = Dataset(root_dir=ROOT_DIR,
+                      batch_size=1, validation=True)
+
+    _, _, test_dataset = dataset.make()
+
+    if choice == 1:
+        predict(model, test_dataset)
+    elif choice == 2:
+        loss, accuracy = predict(model, test_dataset)
+        for image in test_dataset.take(10):
+            show_predictions(model, image[0][0], image[1][0])
+
+
 if __name__ == "__main__":
-    print("Choose:")
+    print("Select one Option:")
     print("1. Train Model")
+    print("2. Predict Model")
     choice = int(input("Choice: "))
     if choice == 1:
         train_model()
+    if choice == 2:
+        predict_model()
