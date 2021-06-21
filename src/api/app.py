3d7ad1ba4bbb981ca55f4e147630fa00439c76bd
TimilsinaBimal <timilsina.bml05@gmail.com>
2021-06-21 19:29:14 +0545

    Flask api

---
+++ src/api/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_restful import Api, Resource
+
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class BlurImage(Resource):
+    def get(self):
+        return {"bimal": "hello"}
+
+
+api.add_resource(BlurImage, "/result/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
