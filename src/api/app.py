6ccaadeae24f17482fd290c643b3ddda5fef8674
TimilsinaBimal <timilsina.bml05@gmail.com>
2021-06-28 14:35:54 +0545

    API Build

--- src/api/app.py
+++ src/api/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restful import Api, Resource
-
+from api.preprocess import get_image_data, prepare_image, predict, blur_image, BASE_URL
+import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 api = Api(app)
@@ -8,10 +9,16 @@ api = Api(app)
 
 class BlurImage(Resource):
     def get(self):
-        return {"bimal": "hello"}
+        # original_image = get_image_data(BASE_URL)
+        original_image = prepare_image("src/test/test.JPG")
+        predicted_mask = predict(original_image)
+        plt.imshow(predicted_mask)
+        blurred_image = blur_image(original_image, predicted_mask)
+        return {"original_image": [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+                "predicted_image": predicted_mask.numpy().tolist()}
 
 
 api.add_resource(BlurImage, "/result/")
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
--- src/api/preprocess.py
+++ src/api/preprocess.py
@@ -2,27 +2,43 @@ import requests
 import json
 import numpy as np
 import tensorflow as tf
+import os
+from pathlib import Path
+from models.train import train_segnet
 
-BASE_URL = "127.0.0.1:5500/result/"
+BASE_URL = "http://127.0.0.1:5000/result/"
 IMAGE_HEIGHT = IMAGE_WIDTH = 224
-
-
-def prepare_image(img):
-    img = tf.io.decode_image(img, channels=3, dtype=tf.float32)
-    img = tf.image.resize(img, [IMAGE_HEIGHT, IMAGE_WIDTH])
-    return img[np.newaxis, ...]
 
 
 def get_image_data(base_url):
     response = requests.get(base_url)
     json_data = json.loads(response.content.decode())
     image = json_data["original_image"]
-    image = list(image)
     image = np.array(image)
     return image
 
 
-def get_mask(image, model):
-    image = prepare_image(image)
+def prepare_image(file_path):
+    img = tf.io.read_file(file_path)
+    img = tf.io.decode_image(img, channels=3, dtype=tf.float32)
+    img = tf.image.resize(img, [IMAGE_HEIGHT, IMAGE_WIDTH])
+    return img[np.newaxis, ...]
 
-    return image
+
+def preprocess_mask(pred_mask):
+    pred_mask = tf.argmax(pred_mask, axis=-1)
+    pred_mask = pred_mask[..., tf.newaxis]
+    return pred_mask[0]
+
+
+def predict(img):
+    model = train_segnet()
+    checkpoint = tf.train.Checkpoint(model)
+    checkpoint_path = "models/SegNet/"
+    checkpoint.restore(checkpoint_path)
+    predicted_mask = model.predict(img)
+    return preprocess_mask(predicted_mask)
+
+
+def blur_image(original_image, predicted_mask):
+    pass
