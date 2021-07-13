c9d4ffbfcaa548bd9ffbc78cf143087330450141
TimilsinaBimal <timilsina.bml05@gmail.com>
2021-07-13 10:46:52 +0545

    Blurred Image Visualization

--- src/visualization/image.py
+++ src/visualization/image.py
@@ -1,12 +1,23 @@
 import matplotlib.pyplot as plt
 
 
-def display(display_list):
-    plt.figure(figsize=(15, 15))
+def display(display_list, iou=0):
+    plt.figure(figsize=(10, 5))
     title = ['Input Image', 'True Mask', 'Predicted Mask']
     for i in range(len(display_list)):
         plt.subplot(1, len(display_list), i+1)
-        plt.title(title[i])
+        plt.title(title[i] + "  IOU: %.2f" % iou)
+        plt.imshow(display_list[i])
+        plt.axis('off')
+    plt.show()
+
+
+def display_blur(display_list, iou=0):
+    plt.figure(figsize=(10, 5))
+    title = ['Input Image', 'True Mask', 'Predicted Mask', 'Blur Image']
+    for i in range(len(display_list)):
+        plt.subplot(1, len(display_list), i+1)
+        plt.title(title[i] + "  IOU: %.2f" % iou)
         plt.imshow(display_list[i])
         plt.axis('off')
     plt.show()
